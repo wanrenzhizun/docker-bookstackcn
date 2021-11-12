c287019662bd0fb0530a04404839ffd8a34b3122
wanrenzhizun <31756438+wanrenzhizun@users.noreply.github.com>
2021-11-13 01:52:25 +0800

    Update build_node.sh

--- build_node.sh
+++ build_node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-BOOKSTACK_VERSION=v2.9
-docker build -t willdockerhub/bookstack:${BOOKSTACK_VERSION}_node -f Dockerfile.node .
+BOOKSTACK_VERSION=v2.10
+docker build -t wanrenzhizun/bookstack:${BOOKSTACK_VERSION}_node -f Dockerfile.node .
